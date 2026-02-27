85ed5e3769bee8ecad3f0f220d64440c503c0311
LimDoHyeon <do-hyeon@gm.gist.ac.kr>
2026-02-27 17:47:06 +0900

    code backbone

---
+++ src/datamodule/dataloader.py
@@ -0,0 +1,15 @@
+
+# TUT2018, TAU2019
+CLASSES = ["airport", "shopping_mall", "metro_station", "street_pedestrian",
+              "public_square", "street_traffic", "tram", "bus", "metro", "park"]
+
+CLASSES_COCHL = ["Bus", "SubwayStation", "Street", "Park"]
+
+
+## Dataloaders
+class SingleDataLoader():
+    pass
+
+
+class AllDataLoader():
+    pass
---
+++ src/datamodule/dataset.py
@@ -0,0 +1,27 @@
+# 6 Europe
+# Lisbon
+# Lyon
+# Prague
+# Korea
+
+
+## Domain-wise dataset
+class Europe6Dataset():
+    pass
+
+class LisbonDataset():
+    pass
+
+class LyonDataset():
+    pass
+
+class PragueDataset():
+    pass
+
+class KoreaDataset():
+    pass
+
+
+## Whole Domains in one Dataset - not for incremental learning
+class AllDataset(): # i.e. collator
+    pass
