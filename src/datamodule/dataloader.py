95244a1ef87b877f85d20f64e3123ce8c3f299e7
LimDoHyeon <do-hyeon@gm.gist.ac.kr>
2026-02-27 18:25:08 +0900

    remove cochl-only class dict

--- src/datamodule/dataloader.py
+++ src/datamodule/dataloader.py
@@ -1,9 +1,9 @@
 
-# TUT2018, TAU2019
-CLASSES = ["airport", "shopping_mall", "metro_station", "street_pedestrian",
-              "public_square", "street_traffic", "tram", "bus", "metro", "park"]
 
-CLASSES_COCHL = ["Bus", "SubwayStation", "Street", "Park"]
+# TUT2018, TAU2019
+# NOTE: cochl's different class-names are converted correctly based on below.
+CLASSES = ["airport", "shopping_mall", "metro_station", "street_pedestrian",
+           "public_square", "street_traffic", "tram", "bus", "metro", "park"]
 
 
 ## Dataloaders
